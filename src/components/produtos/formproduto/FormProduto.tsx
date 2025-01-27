a217d58391f807e08430ce135ed42c3f3d1790f9
Kagradiel <matheusp_torres@hotmail.com>
2025-01-27 13:41:28 -0300

    extras: mobile, redesign, produtos

--- src/components/produtos/formproduto/FormProduto.tsx
+++ src/components/produtos/formproduto/FormProduto.tsx
@@ -7,13 +7,10 @@ import Categoria from "../../../models/categoria/Categoria";
 
 const FormProdutos = () => {
   const navigate = useNavigate();
-
   const [produto, setProduto] = useState<Produto>({} as Produto);
   const [categorias, setCategorias] = useState<Categoria[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
-
   const { id } = useParams<{ id: string }>();
-
 
   async function buscarPorId(id: string) {
     try {
@@ -24,7 +21,6 @@ const FormProdutos = () => {
       }
     }
   }
-
 
   async function buscarCategorias() {
     try {
@@ -43,8 +39,9 @@ const FormProdutos = () => {
     buscarCategorias();
   }, [id]);
 
- 
-  function atualizarEstado(e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) {
+  function atualizarEstado(
+    e: ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) {
     setProduto({
       ...produto,
       [e.target.name]: e.target.value,
@@ -55,7 +52,6 @@ const FormProdutos = () => {
     navigate("/produtos");
   }
 
-  
   async function gerarNovoProduto(e: ChangeEvent<HTMLFormElement>) {
     e.preventDefault();
     setIsLoading(true);
@@ -85,102 +81,96 @@ const FormProdutos = () => {
   }
 
   return (
-    <div className="container flex flex-col items-center justify-center mx-auto">
-      <h1 className="text-4xl text-center my-8">
-        {id === undefined ? "Cadastrar Produto" : "Editar Produto"}
-      </h1>
+    <div className="fixed inset-0 flex justify-center items-center bg-opacity-0">
+      <div className="w-full max-w-md p-6 bg-white rounded-lg shadow-md">
+        <h1 className="text-2xl text-center mb-6">
+          {id === undefined ? "Cadastrar Produto" : "Editar Produto"}
+        </h1>
 
-      <form
-        className="w-1/2 flex flex-col gap-4 p-8 bg-white rounded-xl shadow-md"
-        onSubmit={gerarNovoProduto}
-      >
-        
-        <div className="flex flex-col gap-2">
-          <label htmlFor="nome" className="font-semibold">
-            Nome do Produto
-          </label>
-          <input
-            type="text"
-            placeholder="Nome do produto"
-            name="nome"
-            className="border-2 border-slate-700 rounded-md p-3"
-            value={produto.nome || ""}
-            onChange={atualizarEstado}
-          />
-        </div>
+        <form className="flex flex-col gap-4" onSubmit={gerarNovoProduto}>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="nome" className="font-semibold">
+              Nome do Produto
+            </label>
+            <input
+              type="text"
+              placeholder="Nome do produto"
+              name="nome"
+              className="border-2 border-slate-700 rounded-md p-3"
+              value={produto.nome || ""}
+              onChange={atualizarEstado}
+            />
+          </div>
 
-        
-        <div className="flex flex-col gap-2">
-          <label htmlFor="preco" className="font-semibold">
-            Preço
-          </label>
-          <input
-            type="number"
-            placeholder="Preço do produto"
-            name="preco"
-            className="border-2 border-slate-700 rounded-md p-3"
-            value={produto.preco || ""}
-            onChange={atualizarEstado}
-          />
-        </div>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="preco" className="font-semibold">
+              Preço
+            </label>
+            <input
+              type="number"
+              placeholder="Preço do produto"
+              name="preco"
+              className="border-2 border-slate-700 rounded-md p-3"
+              value={produto.preco || ""}
+              onChange={atualizarEstado}
+            />
+          </div>
 
-        
-        <div className="flex flex-col gap-2">
-          <label htmlFor="categoria" className="font-semibold">
-            Categoria
-          </label>
-          <select
-            name="categoria"
-            className="border-2 border-slate-700 rounded-md p-3"
-            value={produto.categoria?.id || ""}
-            onChange={atualizarEstado}
+          <div className="flex flex-col gap-2">
+            <label htmlFor="categoria" className="font-semibold">
+              Categoria
+            </label>
+            <select
+              name="categoria"
+              className="border-2 border-slate-700 rounded-md p-3"
+              value={produto.categoria?.id} 
+              onChange={atualizarEstado}
+            >
+              <option value="">Selecione a categoria</option>
+              {categorias.map((categoria) => (
+                <option key={categoria.id} value={categoria.id}>
+                  {categoria.nome}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <label htmlFor="foto" className="font-semibold">
+              Foto do Produto
+            </label>
+            <input
+              type="text"
+              placeholder="URL da foto"
+              name="foto"
+              className="border-2 border-slate-700 rounded-md p-3"
+              value={produto.foto || ""}
+              onChange={atualizarEstado}
+            />
+          </div>
+
+          <button
+            className="w-full py-2 rounded-md text-white bg-lime-700 hover:bg-lime-900 mt-4 flex items-center justify-center cursor-pointer"
+            type="submit"
           >
-            <option value="">Selecione a categoria</option>
-            {categorias.map((categoria) => (
-              <option key={categoria.id} value={categoria.id}>
-                {categoria.nome}
-              </option>
-            ))}
-          </select>
-        </div>
+            {isLoading ? (
+              <div className="flex justify-center">
+                <MutatingDots color="#ffffff" secondaryColor="#5a5a5a" />
+              </div>
+            ) : (
+              <span>{id === undefined ? "Cadastrar" : "Atualizar"}</span>
+            )}
+          </button>
 
-        
-        <div className="flex flex-col gap-2">
-          <label htmlFor="foto" className="font-semibold">
-            Foto do Produto
-          </label>
-          <input
-            type="text"
-            placeholder="URL da foto"
-            name="foto"
-            className="border-2 border-slate-700 rounded-md p-3"
-            value={produto.foto || ""}
-            onChange={atualizarEstado}
-          />
-        </div>
-
-        
-        <button
-          className="w-full py-2 rounded-md text-white bg-lime-700 hover:bg-lime-900 mt-4 flex items-center justify-center cursor-pointer"
-          type="submit"
-        >
-          {isLoading ? (
-            <div className="flex justify-center">
-              <MutatingDots color="#ffffff" secondaryColor="#5a5a5a" />
-            </div>
-          ) : (
-            <span>{id === undefined ? "Cadastrar" : "Atualizar"}</span>
-          )}
-        </button>
-
-        <button
-          className="w-full py-2 rounded-md text-white bg-gray-500 hover:bg-gray-700 mt-4 flex items-center justify-center cursor-pointer"
-          type="button"
-          onClick={retornar}
-        >
-          Voltar
-        </button>
-      </form>
+          <button
+            className="w-full py-2 rounded-md text-white bg-gray-500 hover:bg-gray-700 mt-4 flex items-center justify-center cursor-pointer"
+            type="button"
+            onClick={retornar}
+          >
+            Voltar
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
